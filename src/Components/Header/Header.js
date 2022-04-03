4bbe10483e0d5aa97852c9fc0ac8beb25c6772e0
shakilahmed09 <shakilahmed.pure@gmail.com>
2022-04-04 00:26:01 +0600

    navbar added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import CustomLink from '../CustomLink/CustomLink';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <nav className='navbar'>
+                <CustomLink to='/'>Home</CustomLink>
+                <CustomLink to='/reviews'>Reviews</CustomLink>
+                <CustomLink to='/dashboard'>Dashboard</CustomLink>
+                <CustomLink to='/blogs'>Blogs</CustomLink>
+                <CustomLink to='/about'>About</CustomLink>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
